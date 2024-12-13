4743607cbd1a9aa0391be760f244ff6dbf5e2648
gizaw <gizawag123@gmail.com>
2024-12-13 10:51:43 +0300

    define project model

---
+++ src/app/features/projects/models/project.model.ts
@@ -0,0 +1,12 @@
+export interface Project {
+    id: number; 
+    name: string; 
+    createdDate: string; 
+    createdBy: string; 
+    included: boolean; 
+    integration: string; 
+    description: string; 
+    hours: number; 
+    timeRecords: number; 
+  }
+  
