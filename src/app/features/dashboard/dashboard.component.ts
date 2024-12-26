8e18805c629f013b58cec536579ad1854fcfa6bf
gizaw <gizawag123@gmail.com>
2024-12-26 14:50:18 +0300

    update dashboard component

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { selectLoading } from '../../store/common.selectors';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrl: './dashboard.component.css',
 })
 export class DashboardComponent {
+  loading$: Observable<boolean>;
 
+  constructor(private store: Store) {
+    this.loading$ = this.store.select(selectLoading);
+  }
 }
